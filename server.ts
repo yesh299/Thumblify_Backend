b415b54d8f25159a3cf405564474989594363f36
yesh thakur <tyesh3651@gmail.com>
2026-09-08 23:13:52 +0530

    added the frontend URL in origin

--- server.ts
+++ server.ts
@@ -23,7 +23,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://ai-thumbnail-generator-chi.vercel.app",
+    ],
     credentials: true,
   }),
 );
